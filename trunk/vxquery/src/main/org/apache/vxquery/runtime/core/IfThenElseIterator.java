21d4afcc411477e377f78b58fcbe8178adbf58a4
Vinayak Ravindra Borkar <vinayakb@apache.org>
2009-08-27 17:06:27 +0000

    Minor refactoring
    git-svn-id: https://svn.apache.org/repos/asf/incubator/vxquery@808532 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/vxquery/src/main/org/apache/vxquery/runtime/core/IfThenElseIterator.java
+++ trunk/vxquery/src/main/org/apache/vxquery/runtime/core/IfThenElseIterator.java
@@ -1,19 +1,19 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-* contributor license agreements.  See the NOTICE file distributed with
-* this work for additional information regarding copyright ownership.
-* The ASF licenses this file to You under the Apache License, Version 2.0
-* (the "License"); you may not use this file except in compliance with
-* the License.  You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.vxquery.runtime.core;
 
 import org.apache.vxquery.datamodel.atomic.BooleanValue;
@@ -47,7 +47,7 @@ public class IfThenElseIterator extends AbstractRuntimeIterator {
     public void close(CallStackFrame frame) {
         Boolean c = condition.get(frame);
         if (c != null) {
-            (c ? ti : ei).close(frame);
+            getResultIterator(c).close(frame);
         }
     }
 
@@ -57,9 +57,13 @@ public class IfThenElseIterator extends AbstractRuntimeIterator {
         if (c == null) {
             c = getConditionResult(frame);
             condition.set(frame, c);
-            (c ? ti : ei).open(frame);
+            getResultIterator(c).open(frame);
         }
-        return (c ? ti : ei).next(frame);
+        return getResultIterator(c).next(frame);
+    }
+
+    private RuntimeIterator getResultIterator(Boolean c) {
+        return c ? ti : ei;
     }
 
     @Override
