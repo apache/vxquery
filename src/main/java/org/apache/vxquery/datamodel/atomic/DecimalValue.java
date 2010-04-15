4781bb969f11e68c064e2a6c4c48f7d1244156a3
Till Westmann <tillw@apache.org>
2010-04-15 00:58:40 +0000

    - fixed type annotation
    git-svn-id: https://svn.apache.org/repos/asf/incubator/vxquery/trunk/vxquery@934256 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/vxquery/datamodel/atomic/DecimalValue.java
+++ src/main/java/org/apache/vxquery/datamodel/atomic/DecimalValue.java
@@ -1,19 +1,19 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-* contributor license agreements.  See the NOTICE file distributed with
-* this work for additional information regarding copyright ownership.
-* The ASF licenses this file to You under the Apache License, Version 2.0
-* (the "License"); you may not use this file except in compliance with
-* the License.  You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.vxquery.datamodel.atomic;
 
 import java.math.BigDecimal;
@@ -25,7 +25,7 @@ import org.apache.vxquery.types.BuiltinTypeRegistry;
 
 public class DecimalValue extends NumericValue {
     DecimalValue(BigDecimal value) {
-        super(value, BuiltinTypeRegistry.XS_INTEGER);
+        super(value, BuiltinTypeRegistry.XS_DECIMAL);
     }
 
     public DecimalValue(BigDecimal value, AtomicType type) {
@@ -36,10 +36,10 @@ public class DecimalValue extends NumericValue {
     public CharSequence getStringValue() {
         return stringValue((BigDecimal) value);
     }
-    
+
     static final String stringValue(BigDecimal value) {
         String str = String.valueOf(value);
-        return str.endsWith(".0") ? str.substring(0, str.length() - 2) : str;        
+        return str.endsWith(".0") ? str.substring(0, str.length() - 2) : str;
     }
 
     @Override
