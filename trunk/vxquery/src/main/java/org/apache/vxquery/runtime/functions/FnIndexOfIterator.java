537b9bb1822aefa99c725fbd9193221106d41ea8
Till Westmann <tillw@apache.org>
2010-04-15 00:57:49 +0000

    - use evaluateEagerly() instead of next() to get a single value
    git-svn-id: https://svn.apache.org/repos/asf/incubator/vxquery@934255 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/vxquery/src/main/java/org/apache/vxquery/runtime/functions/FnIndexOfIterator.java
+++ trunk/vxquery/src/main/java/org/apache/vxquery/runtime/functions/FnIndexOfIterator.java
@@ -1,19 +1,19 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-* contributor license agreements.  See the NOTICE file distributed with
-* this work for additional information regarding copyright ownership.
-* The ASF licenses this file to You under the Apache License, Version 2.0
-* (the "License"); you may not use this file except in compliance with
-* the License.  You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.vxquery.runtime.functions;
 
 import java.math.BigInteger;
@@ -40,8 +40,7 @@ public class FnIndexOfIterator extends AbstractLazilyEvaluatedFunctionIterator {
     private final LocalRegisterAccessor<BigInteger> index;
     private final LocalRegisterAccessor<Collation> collation;
 
-    public FnIndexOfIterator(RegisterAllocator rAllocator, Function fn, RuntimeIterator[] arguments,
-            StaticContext ctx) {
+    public FnIndexOfIterator(RegisterAllocator rAllocator, Function fn, RuntimeIterator[] arguments, StaticContext ctx) {
         super(rAllocator, fn, arguments, ctx);
         value = new LocalRegisterAccessor<XDMAtomicValue>(rAllocator.allocate(1));
         index = new LocalRegisterAccessor<BigInteger>(rAllocator.allocate(1));
@@ -57,7 +56,7 @@ public class FnIndexOfIterator extends AbstractLazilyEvaluatedFunctionIterator {
     public Object next(CallStackFrame frame) throws SystemException {
         final RuntimeControlBlock rcb = frame.getRuntimeControlBlock();
         if (value.get(frame) == null) {
-            value.set(frame, (XDMAtomicValue) arguments[1].next(frame));
+            value.set(frame, (XDMAtomicValue) arguments[1].evaluateEagerly(frame));
             Collation c;
             if (arguments.length > 2) {
                 XDMItem cv = RuntimeUtils.fetchItemEagerly(arguments[2], frame);
@@ -78,8 +77,8 @@ public class FnIndexOfIterator extends AbstractLazilyEvaluatedFunctionIterator {
             }
             BigInteger idx = index.get(frame).add(BigInteger.ONE);
             index.set(frame, idx);
-            Boolean res = ComparisonUtils.valueCompare(rcb, v, value.get(frame), ValueEqComparator.INSTANCE,
-                    collation.get(frame));
+            Boolean res = ComparisonUtils.valueCompare(rcb, v, value.get(frame), ValueEqComparator.INSTANCE, collation
+                    .get(frame));
             if (res != null && res) {
                 return rcb.getAtomicValueFactory().createInteger(idx);
             }
