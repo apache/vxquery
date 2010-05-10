88e1282c21d9787bc14dba8f547ae539f8b28b2c
Till Westmann <tillw@apache.org>
2010-05-10 23:30:59 +0000

    fn:doc accepts the empty sequence
    git-svn-id: https://svn.apache.org/repos/asf/incubator/vxquery/trunk/vxquery@942950 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/vxquery/runtime/functions/FnDocIterator.java
+++ src/main/java/org/apache/vxquery/runtime/functions/FnDocIterator.java
@@ -1,25 +1,21 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-* contributor license agreements.  See the NOTICE file distributed with
-* this work for additional information regarding copyright ownership.
-* The ASF licenses this file to You under the Apache License, Version 2.0
-* (the "License"); you may not use this file except in compliance with
-* the License.  You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.vxquery.runtime.functions;
 
 import java.io.IOException;
 import java.io.InputStream;
-
-import org.xml.sax.InputSource;
 
 import org.apache.vxquery.context.DocumentURIResolver;
 import org.apache.vxquery.context.StaticContext;
@@ -33,6 +29,7 @@ import org.apache.vxquery.runtime.RuntimeControlBlock;
 import org.apache.vxquery.runtime.base.AbstractEagerlyEvaluatedFunctionIterator;
 import org.apache.vxquery.runtime.base.RuntimeIterator;
 import org.apache.vxquery.runtime.util.XMLParserUtils;
+import org.xml.sax.InputSource;
 
 public class FnDocIterator extends AbstractEagerlyEvaluatedFunctionIterator {
     public FnDocIterator(RegisterAllocator rAllocator, Function fn, RuntimeIterator[] arguments, StaticContext ctx) {
@@ -42,16 +39,19 @@ public class FnDocIterator extends AbstractEagerlyEvaluatedFunctionIterator {
     @Override
     public Object evaluateEagerly(CallStackFrame frame) throws SystemException {
         XDMAtomicValue uriValue = (XDMAtomicValue) arguments[0].evaluateEagerly(frame);
+        if (uriValue == null) {
+            return null;
+        }
         RuntimeControlBlock rcb = frame.getRuntimeControlBlock();
         String uri = uriValue.getStringValue().toString();
         DocumentURIResolver docUriResolver = rcb.getDynamicContext().getStaticContext().getDataspaceContext()
                 .getDocumentURIResolver();
-        InputStream in;
+        final String baseUri = rcb.getDynamicContext().getStaticContext().getBaseUri();
         try {
-            in = docUriResolver.resolveDocumentURI(uri, rcb.getDynamicContext().getStaticContext().getBaseUri());
+            InputStream in = docUriResolver.resolveDocumentURI(uri, baseUri);
+            return XMLParserUtils.parseInputSource(rcb, new InputSource(in));
         } catch (IOException e) {
             throw new SystemException(ErrorCode.FODC0002, e, uri);
         }
-        return XMLParserUtils.parseInputSource(rcb, new InputSource(in));
     }
 }
