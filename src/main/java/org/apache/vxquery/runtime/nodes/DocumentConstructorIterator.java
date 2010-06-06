fe386e6212c8e37b5750dc8aa7403dbfde504ada
Till Westmann <tillw@apache.org>
2010-06-06 20:07:53 +0000

    - added support for more than one item in the content sequence
    git-svn-id: https://svn.apache.org/repos/asf/incubator/vxquery/trunk/vxquery@951949 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/vxquery/runtime/nodes/DocumentConstructorIterator.java
+++ src/main/java/org/apache/vxquery/runtime/nodes/DocumentConstructorIterator.java
@@ -1,28 +1,29 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-* contributor license agreements.  See the NOTICE file distributed with
-* this work for additional information regarding copyright ownership.
-* The ASF licenses this file to You under the Apache License, Version 2.0
-* (the "License"); you may not use this file except in compliance with
-* the License.  You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.vxquery.runtime.nodes;
 
 import org.apache.vxquery.datamodel.NodeConstructingEventAcceptor;
 import org.apache.vxquery.datamodel.NodeFactory;
 import org.apache.vxquery.datamodel.XDMItem;
+import org.apache.vxquery.datamodel.XDMSequence;
+import org.apache.vxquery.datamodel.XDMValue;
 import org.apache.vxquery.exceptions.SystemException;
 import org.apache.vxquery.runtime.CallStackFrame;
 import org.apache.vxquery.runtime.RegisterAllocator;
 import org.apache.vxquery.runtime.base.AbstractEagerlyEvaluatedIterator;
+import org.apache.vxquery.runtime.base.CloseableIterator;
 import org.apache.vxquery.runtime.base.RuntimeIterator;
 
 public class DocumentConstructorIterator extends AbstractEagerlyEvaluatedIterator {
@@ -39,8 +40,20 @@ public class DocumentConstructorIterator extends AbstractEagerlyEvaluatedIterato
         NodeConstructingEventAcceptor dc = nf.createDocumentConstructor();
         dc.open();
         dc.startDocument();
-        XDMItem item = (XDMItem) ci.evaluateEagerly(frame);
-        dc.item(item);
+        XDMValue xdmv = (XDMValue) ci.evaluateEagerly(frame);
+        if (xdmv != null) {
+            if (xdmv.getDMOKind().isItem()) {
+                dc.item((XDMItem) xdmv);
+            } else {
+                XDMSequence xdms = (XDMSequence) xdmv;
+                CloseableIterator iter = xdms.createItemIterator();
+                XDMItem xdmi;
+                while ((xdmi = (XDMItem) iter.next()) != null) {
+                    dc.item(xdmi);
+                }
+                iter.close();
+            }
+        }
         dc.endDocument();
         dc.close();
         return dc.getConstructedNode();
