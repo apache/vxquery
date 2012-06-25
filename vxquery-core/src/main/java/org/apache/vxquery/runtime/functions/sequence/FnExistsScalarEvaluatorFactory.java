eea6dab1430cd61a406ca00d1a3671184de5abcc
Vinayak Ravindra Borkar <vinayakb@apache.org>
2012-06-25 10:59:07 +0000

    Added fn:exists, fn:empty, fn:boolean
    git-svn-id: https://svn.apache.org/repos/asf/incubator/vxquery/branches/vxquery_algebricks@1353456 13f79535-47bb-0310-9956-ffa450edef68

--- vxquery-core/src/main/java/org/apache/vxquery/runtime/functions/sequence/FnExistsScalarEvaluatorFactory.java
+++ vxquery-core/src/main/java/org/apache/vxquery/runtime/functions/sequence/FnExistsScalarEvaluatorFactory.java
@@ -1,42 +1,41 @@
-package org.apache.vxquery.runtime.functions.bool;
+package org.apache.vxquery.runtime.functions.sequence;
 
+import org.apache.vxquery.datamodel.accessors.SequencePointable;
 import org.apache.vxquery.datamodel.accessors.TaggedValuePointable;
 import org.apache.vxquery.datamodel.values.ValueTag;
 import org.apache.vxquery.exceptions.SystemException;
 import org.apache.vxquery.runtime.functions.base.AbstractTaggedValueArgumentScalarEvaluator;
 import org.apache.vxquery.runtime.functions.base.AbstractTaggedValueArgumentScalarEvaluatorFactory;
+import org.apache.vxquery.runtime.functions.bool.FnFalseScalarEvaluatorFactory;
+import org.apache.vxquery.runtime.functions.bool.FnTrueScalarEvaluatorFactory;
 
 import edu.uci.ics.hyracks.algebricks.common.exceptions.AlgebricksException;
 import edu.uci.ics.hyracks.algebricks.runtime.base.IScalarEvaluator;
 import edu.uci.ics.hyracks.algebricks.runtime.base.IScalarEvaluatorFactory;
 import edu.uci.ics.hyracks.data.std.api.IPointable;
-import edu.uci.ics.hyracks.data.std.primitive.BooleanPointable;
 
-public class FnTrueScalarEvaluatorFactory extends AbstractTaggedValueArgumentScalarEvaluatorFactory {
+public class FnExistsScalarEvaluatorFactory extends AbstractTaggedValueArgumentScalarEvaluatorFactory {
     private static final long serialVersionUID = 1L;
 
-    private static final byte[] BOOLEAN_TRUE_CONSTANT;
-
-    static {
-        BOOLEAN_TRUE_CONSTANT = new byte[2];
-        BOOLEAN_TRUE_CONSTANT[0] = ValueTag.XS_BOOLEAN_TAG;
-        BooleanPointable.setBoolean(BOOLEAN_TRUE_CONSTANT, 1, true);
-    }
-
-    public static void setTrue(IPointable p) {
-        p.set(BOOLEAN_TRUE_CONSTANT, 0, 2);
-    }
-
-    public FnTrueScalarEvaluatorFactory(IScalarEvaluatorFactory[] args) {
+    public FnExistsScalarEvaluatorFactory(IScalarEvaluatorFactory[] args) {
         super(args);
     }
 
     @Override
     protected IScalarEvaluator createEvaluator(IScalarEvaluator[] args) throws AlgebricksException {
+        final SequencePointable seqp = new SequencePointable();
         return new AbstractTaggedValueArgumentScalarEvaluator(args) {
             @Override
             protected void evaluate(TaggedValuePointable[] args, IPointable result) throws SystemException {
-                result.set(BOOLEAN_TRUE_CONSTANT, 0, 2);
+                TaggedValuePointable tvp = args[0];
+                if (tvp.getTag() == ValueTag.SEQUENCE_TAG) {
+                    tvp.getValue(seqp);
+                    if (seqp.getEntryCount() == 0) {
+                        FnFalseScalarEvaluatorFactory.setFalse(result);
+                        return;
+                    }
+                }
+                FnTrueScalarEvaluatorFactory.setTrue(result);
             }
         };
     }
