d5cbb598d32ad1698182b2ee0c173408a17e3d53
Vinayak Ravindra Borkar <vinayakb@apache.org>
2012-06-26 00:35:38 +0000

    Modified evaluator factories to receive task context
    git-svn-id: https://svn.apache.org/repos/asf/incubator/vxquery@1353785 13f79535-47bb-0310-9956-ffa450edef68

--- branches/vxquery_algebricks/vxquery-core/src/main/java/org/apache/vxquery/runtime/functions/base/AbstractTaggedValueArgumentAggregateEvaluatorFactory.java
+++ branches/vxquery_algebricks/vxquery-core/src/main/java/org/apache/vxquery/runtime/functions/base/AbstractTaggedValueArgumentAggregateEvaluatorFactory.java
@@ -5,6 +5,7 @@ import edu.uci.ics.hyracks.algebricks.runtime.base.IAggregateEvaluator;
 import edu.uci.ics.hyracks.algebricks.runtime.base.IAggregateEvaluatorFactory;
 import edu.uci.ics.hyracks.algebricks.runtime.base.IScalarEvaluator;
 import edu.uci.ics.hyracks.algebricks.runtime.base.IScalarEvaluatorFactory;
+import edu.uci.ics.hyracks.api.context.IHyracksTaskContext;
 
 public abstract class AbstractTaggedValueArgumentAggregateEvaluatorFactory implements IAggregateEvaluatorFactory {
     private static final long serialVersionUID = 1L;
@@ -16,10 +17,10 @@ public abstract class AbstractTaggedValueArgumentAggregateEvaluatorFactory imple
     }
 
     @Override
-    public final IAggregateEvaluator createAggregateEvaluator() throws AlgebricksException {
+    public final IAggregateEvaluator createAggregateEvaluator(IHyracksTaskContext ctx) throws AlgebricksException {
         IScalarEvaluator[] es = new IScalarEvaluator[args.length];
         for (int i = 0; i < es.length; ++i) {
-            es[i] = args[i].createScalarEvaluator();
+            es[i] = args[i].createScalarEvaluator(ctx);
         }
         return createEvaluator(es);
     }
