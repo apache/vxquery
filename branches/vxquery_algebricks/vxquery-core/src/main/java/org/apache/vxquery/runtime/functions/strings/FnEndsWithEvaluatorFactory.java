9062afd1493d197f6db48d39a48c77cc32f70443
Till Westmann <tillw@apache.org>
2012-07-08 10:32:34 +0000

    add implementation for fn:ends-with as attached to VXQUERY-35
    git-svn-id: https://svn.apache.org/repos/asf/incubator/vxquery@1358717 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branches/vxquery_algebricks/vxquery-core/src/main/java/org/apache/vxquery/runtime/functions/strings/FnEndsWithEvaluatorFactory.java
@@ -0,0 +1,102 @@
+package org.apache.vxquery.runtime.functions.strings;
+
+import org.apache.vxquery.datamodel.accessors.TaggedValuePointable;
+import org.apache.vxquery.datamodel.values.ValueTag;
+import org.apache.vxquery.exceptions.ErrorCode;
+import org.apache.vxquery.exceptions.SystemException;
+import org.apache.vxquery.runtime.functions.base.AbstractTaggedValueArgumentScalarEvaluator;
+import org.apache.vxquery.runtime.functions.base.AbstractTaggedValueArgumentScalarEvaluatorFactory;
+
+import edu.uci.ics.hyracks.algebricks.common.exceptions.AlgebricksException;
+import edu.uci.ics.hyracks.algebricks.runtime.base.IScalarEvaluator;
+import edu.uci.ics.hyracks.algebricks.runtime.base.IScalarEvaluatorFactory;
+import edu.uci.ics.hyracks.api.context.IHyracksTaskContext;
+import edu.uci.ics.hyracks.data.std.api.IPointable;
+import edu.uci.ics.hyracks.data.std.primitive.UTF8StringPointable;
+
+public class FnEndsWithEvaluatorFactory extends AbstractTaggedValueArgumentScalarEvaluatorFactory {
+    private static final long serialVersionUID = 1L;
+
+    public FnEndsWithEvaluatorFactory(IScalarEvaluatorFactory[] args) {
+        super(args);
+    }
+
+    @Override
+    protected IScalarEvaluator createEvaluator(IHyracksTaskContext ctx, IScalarEvaluator[] args)
+            throws AlgebricksException {
+        final UTF8StringPointable stringp1 = new UTF8StringPointable();
+        final UTF8StringPointable stringp2 = new UTF8StringPointable();
+        final UTF8StringPointable stringp3 = new UTF8StringPointable();
+        final ICharacterIterator charIterator1 = new UTF8StringCharacterIterator(stringp1);
+        final ICharacterIterator charIterator2 = new UTF8StringCharacterIterator(stringp2);
+
+        return new AbstractTaggedValueArgumentScalarEvaluator(args) {
+            @Override
+            protected void evaluate(TaggedValuePointable[] args, IPointable result) throws SystemException {
+                // Default result is false.
+                byte[] booleanResult = new byte[2];
+                booleanResult[0] = ValueTag.XS_BOOLEAN_TAG;
+                booleanResult[1] = 0;
+
+                TaggedValuePointable tvp1 = args[0];
+                TaggedValuePointable tvp2 = args[1];
+
+                // Only accept strings as input.
+                if (tvp1.getTag() != ValueTag.XS_STRING_TAG) {
+                    throw new SystemException(ErrorCode.FORG0006);
+                }
+                if (tvp2.getTag() != ValueTag.XS_STRING_TAG) {
+                    throw new SystemException(ErrorCode.FORG0006);
+                }
+                tvp1.getValue(stringp1);
+                tvp2.getValue(stringp2);
+                int stringLength1 = stringp1.getStringLength();
+                int stringLength2 = stringp2.getStringLength();
+                charIterator1.reset();
+                charIterator2.reset();
+
+                // Third parameter is optional.
+                if (args.length > 2) {
+                    TaggedValuePointable tvp3 = args[2];
+                    if (tvp3.getTag() != ValueTag.XS_STRING_TAG) {
+                        throw new SystemException(ErrorCode.FORG0006);
+                    }
+                    tvp3.getValue(stringp3);
+                }
+                // TODO use the third value as collation
+
+                int stringCharOffset = stringLength1 - stringLength2;
+
+                // Only check if stringp2 can fit into stringp1.
+                if (stringCharOffset >= 0) {
+                    // Only need to run comparisons if they both have a non empty string.
+                    if (stringLength1 > 0 && stringLength2 > 0) {
+                        // Move string one's cursor for comparison
+                        for (; stringCharOffset > 0; --stringCharOffset) {
+                            charIterator1.next();
+                        }
+                        int c1;
+                        int c2;
+                        while (true) {
+                            c1 = charIterator1.next();
+                            c2 = charIterator2.next();
+                            if (c1 != c2) {
+                                // Characters do not match
+                                break;
+                            }
+                            if (c1 == ICharacterIterator.EOS_CHAR && c2 == ICharacterIterator.EOS_CHAR) {
+                                // Checked the full length of search string.
+                                booleanResult[1] = 1;
+                                break;
+                            }
+                        }
+                    } else if (stringLength2 == 0) {
+                        booleanResult[1] = 1;
+                    }
+                }
+
+                result.set(booleanResult, 0, 2);
+            }
+        };
+    }
+}
