09de4c76ab9c9d026b5c22c6aa7b314fdc8b843e
prestonc <prestonc@unknown>
2012-07-11 17:14:23 +0000

    Updated the substring character iterator to improve performance and correct the parameters passed into the function.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/vxquery/branches/vxquery_algebricks@1360304 13f79535-47bb-0310-9956-ffa450edef68

--- vxquery-core/src/main/java/org/apache/vxquery/runtime/functions/strings/SubstringCharacterIterator.java
+++ vxquery-core/src/main/java/org/apache/vxquery/runtime/functions/strings/SubstringCharacterIterator.java
@@ -1,39 +1,39 @@
 package org.apache.vxquery.runtime.functions.strings;
 
-
 public class SubstringCharacterIterator implements ICharacterIterator {
     private final ICharacterIterator in;
     private int start;
-    private int end;
+    private int length;
     private int charOffset;
-    
+
     public SubstringCharacterIterator(ICharacterIterator in) {
         this.in = in;
     }
 
-    public void setBounds(int start, int end) {
+    public void setBounds(int start, int length) {
         this.start = start;
-        this.end = end;
+        this.length = length;
     }
 
     @Override
     public char next() {
         char c = in.next();
-        if (charOffset <= start) {
+        // Only drill down if there is more to the string.
+        if (c == ICharacterIterator.EOS_CHAR) {
+            return c;
+        } else if (charOffset < start) {
             ++charOffset;
             return next();
-        }
-        else if (charOffset <= end) {
+        } else if (charOffset < start + length) {
             ++charOffset;
             return c;
         }
-        else {
-            return (char) 0;
-        }
+        return (char) 0;
     }
 
     @Override
     public void reset() {
+        in.reset();
         charOffset = 0;
     }
 
