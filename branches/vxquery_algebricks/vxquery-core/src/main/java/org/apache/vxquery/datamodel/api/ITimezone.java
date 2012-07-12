2d1a7f32041bab31e401a498f10164ac86598c8f
prestonc <prestonc@unknown>
2012-07-12 02:38:06 +0000

    The update contains a new function to apply the timezone to a date, datetime or time pointable through a timezone interface.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/vxquery@1360514 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branches/vxquery_algebricks/vxquery-core/src/main/java/org/apache/vxquery/datamodel/api/ITimezone.java
@@ -0,0 +1,14 @@
+package org.apache.vxquery.datamodel.api;
+
+public interface ITimezone {
+    public long getTimezoneHour();
+
+    public long getTimezoneMinute();
+
+    public long getTimezone();
+
+    public long getYearMonth();
+
+    public long getDayTime();
+
+}
