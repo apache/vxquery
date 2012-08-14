e6f2458b13fdf161f97a27d8527c7ea69a99be81
prestonc <prestonc@unknown>
2012-08-14 21:04:41 +0000

    Updated the unsigned values to use one size larger signed storage unit. Updated the arithmetic conversion to allow support this change.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/vxquery@1373099 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/vxquery/vxquery-core/src/main/java/org/apache/vxquery/runtime/functions/cast/CastToUnsignedIntOperation.java
+++ trunk/vxquery/vxquery-core/src/main/java/org/apache/vxquery/runtime/functions/cast/CastToUnsignedIntOperation.java
@@ -2,7 +2,7 @@ package org.apache.vxquery.runtime.functions.cast;
 
 import org.apache.vxquery.datamodel.values.ValueTag;
 
-public class CastToUnsignedIntOperation extends CastToIntOperation {
+public class CastToUnsignedIntOperation extends CastToLongOperation {
     boolean negativeAllowed = false;
     int returnTag = ValueTag.XS_UNSIGNED_INT_TAG;
 }
--- trunk/vxquery/vxquery-core/src/main/java/org/apache/vxquery/runtime/functions/cast/CastToUnsignedShortOperation.java
+++ trunk/vxquery/vxquery-core/src/main/java/org/apache/vxquery/runtime/functions/cast/CastToUnsignedShortOperation.java
@@ -2,7 +2,7 @@ package org.apache.vxquery.runtime.functions.cast;
 
 import org.apache.vxquery.datamodel.values.ValueTag;
 
-public class CastToUnsignedShortOperation extends CastToShortOperation {
+public class CastToUnsignedShortOperation extends CastToIntOperation {
     boolean negativeAllowed = false;
     int returnTag = ValueTag.XS_UNSIGNED_SHORT_TAG;
 }
