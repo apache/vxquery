1c1b5082190218004cad72f59a798ab853bfd584
prestonc <prestonc@unknown>
2012-09-12 23:50:19 +0000

    Added fn:distinct-values.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/vxquery/trunk/vxquery@1384160 13f79535-47bb-0310-9956-ffa450edef68

---
+++ vxquery-core/src/main/java/org/apache/vxquery/runtime/functions/sequence/FnDistinctValuesScalarEvaluatorFactory.java
@@ -0,0 +1,119 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.vxquery.runtime.functions.sequence;
+
+import java.io.IOException;
+
+import org.apache.vxquery.context.DynamicContext;
+import org.apache.vxquery.datamodel.accessors.SequencePointable;
+import org.apache.vxquery.datamodel.accessors.TaggedValuePointable;
+import org.apache.vxquery.datamodel.builders.sequence.SequenceBuilder;
+import org.apache.vxquery.datamodel.values.ValueTag;
+import org.apache.vxquery.exceptions.ErrorCode;
+import org.apache.vxquery.exceptions.SystemException;
+import org.apache.vxquery.runtime.functions.base.AbstractTaggedValueArgumentScalarEvaluator;
+import org.apache.vxquery.runtime.functions.base.AbstractTaggedValueArgumentScalarEvaluatorFactory;
+import org.apache.vxquery.runtime.functions.comparison.AbstractValueComparisonOperation;
+import org.apache.vxquery.runtime.functions.comparison.AbstractValueComparisonScalarEvaluatorFactory;
+import org.apache.vxquery.runtime.functions.comparison.ValueEqComparisonOperation;
+
+import edu.uci.ics.hyracks.algebricks.common.exceptions.AlgebricksException;
+import edu.uci.ics.hyracks.algebricks.runtime.base.IScalarEvaluator;
+import edu.uci.ics.hyracks.algebricks.runtime.base.IScalarEvaluatorFactory;
+import edu.uci.ics.hyracks.api.context.IHyracksTaskContext;
+import edu.uci.ics.hyracks.data.std.api.IPointable;
+import edu.uci.ics.hyracks.data.std.primitive.UTF8StringPointable;
+import edu.uci.ics.hyracks.data.std.primitive.VoidPointable;
+import edu.uci.ics.hyracks.data.std.util.ArrayBackedValueStorage;
+
+public class FnDistinctValuesScalarEvaluatorFactory extends AbstractTaggedValueArgumentScalarEvaluatorFactory {
+    private static final long serialVersionUID = 1L;
+
+    public FnDistinctValuesScalarEvaluatorFactory(IScalarEvaluatorFactory[] args) {
+        super(args);
+    }
+
+    @Override
+    protected IScalarEvaluator createEvaluator(IHyracksTaskContext ctx, IScalarEvaluator[] args)
+            throws AlgebricksException {
+        final ArrayBackedValueStorage abvs = new ArrayBackedValueStorage();
+        final SequenceBuilder sb = new SequenceBuilder();
+        final SequencePointable seq = new SequencePointable();
+        final DynamicContext dCtx = (DynamicContext) ctx.getJobletContext().getGlobalJobData();
+        final AbstractValueComparisonOperation aOp = new ValueEqComparisonOperation();
+        final TaggedValuePointable tvpDistinct = (TaggedValuePointable) TaggedValuePointable.FACTORY.createPointable();
+        final TaggedValuePointable tvpCheck = (TaggedValuePointable) TaggedValuePointable.FACTORY.createPointable();
+        final VoidPointable p = (VoidPointable) VoidPointable.FACTORY.createPointable();
+        final UTF8StringPointable stringp = (UTF8StringPointable) UTF8StringPointable.FACTORY.createPointable();
+
+        return new AbstractTaggedValueArgumentScalarEvaluator(args) {
+            @Override
+            protected void evaluate(TaggedValuePointable[] args, IPointable result) throws SystemException {
+                try {
+                    abvs.reset();
+                    sb.reset(abvs);
+                    TaggedValuePointable tvp1 = args[0];
+                    // Second  parameter is optional.
+                    if (args.length > 1) {
+                        TaggedValuePointable tvp2 = args[1];
+                        if (tvp2.getTag() != ValueTag.XS_STRING_TAG) {
+                            throw new SystemException(ErrorCode.FORG0006);
+                        }
+                        tvp2.getValue(stringp);
+                    }
+                    // TODO use the third value as collation
+
+                    if (tvp1.getTag() == ValueTag.SEQUENCE_TAG) {
+                        tvp1.getValue(seq);
+                        int seqLen = seq.getEntryCount();
+
+                        // Add remaining distinct item.
+                        boolean found = false;
+                        for (int j = 0; j < seqLen; ++j) {
+                            seq.getEntry(j, p);
+                            tvpDistinct.set(p.getByteArray(), p.getStartOffset(), p.getLength());
+
+                            // Check if item is distinct.
+                            if (j + 1 < seqLen) {
+                                for (int k = j + 1; k < seqLen; ++k) {
+                                    seq.getEntry(k, p);
+                                    tvpCheck.set(p.getByteArray(), p.getStartOffset(), p.getLength());
+                                    if (AbstractValueComparisonScalarEvaluatorFactory.compareTaggedValues(aOp,
+                                            tvpDistinct, tvpCheck, dCtx)) {
+                                        found = true;
+                                        break;
+                                    }
+                                }
+                            }
+
+                            // Add distinct item.
+                            if (!found) {
+                                sb.addItem(tvpDistinct);
+                            }
+                        }
+                    } else {
+                        sb.addItem(tvp1);
+                    }
+                    sb.finish();
+                    result.set(abvs);
+                } catch (IOException e) {
+                    throw new SystemException(ErrorCode.SYSE0001);
+                }
+            }
+        };
+    }
+}
