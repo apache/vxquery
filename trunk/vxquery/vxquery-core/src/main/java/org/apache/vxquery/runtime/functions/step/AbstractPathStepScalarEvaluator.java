05bdd5778291c141892e95b181d1c9852220b6f9
prestonc <prestonc@unknown>
2013-08-29 17:49:13 +0000

    Created two new classes to share the code between scalar and unnesting evaluators.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/vxquery@1518726 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/vxquery/vxquery-core/src/main/java/org/apache/vxquery/runtime/functions/step/AbstractPathStepScalarEvaluator.java
+++ trunk/vxquery/vxquery-core/src/main/java/org/apache/vxquery/runtime/functions/step/AbstractPathStepScalarEvaluator.java
@@ -20,25 +20,18 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 import org.apache.vxquery.datamodel.accessors.TaggedValuePointable;
-import org.apache.vxquery.datamodel.accessors.atomic.CodedQNamePointable;
-import org.apache.vxquery.datamodel.accessors.nodes.AttributeNodePointable;
-import org.apache.vxquery.datamodel.accessors.nodes.ElementNodePointable;
 import org.apache.vxquery.datamodel.accessors.nodes.NodeTreePointable;
+import org.apache.vxquery.datamodel.builders.nodes.NodeSubTreeBuilder;
 import org.apache.vxquery.datamodel.builders.sequence.SequenceBuilder;
 import org.apache.vxquery.datamodel.values.ValueTag;
 import org.apache.vxquery.exceptions.SystemException;
 import org.apache.vxquery.runtime.functions.base.AbstractTaggedValueArgumentScalarEvaluator;
-import org.apache.vxquery.types.AttributeType;
-import org.apache.vxquery.types.ElementType;
-import org.apache.vxquery.types.NameTest;
-import org.apache.vxquery.types.NodeType;
+import org.apache.vxquery.runtime.functions.step.NodeTestFilter.INodeFilter;
 import org.apache.vxquery.types.SequenceType;
 
 import edu.uci.ics.hyracks.algebricks.runtime.base.IScalarEvaluator;
 import edu.uci.ics.hyracks.api.context.IHyracksTaskContext;
 import edu.uci.ics.hyracks.data.std.api.IPointable;
-import edu.uci.ics.hyracks.data.std.primitive.UTF8StringPointable;
-import edu.uci.ics.hyracks.data.std.primitive.VoidPointable;
 import edu.uci.ics.hyracks.data.std.util.ArrayBackedValueStorage;
 
 public abstract class AbstractPathStepScalarEvaluator extends AbstractTaggedValueArgumentScalarEvaluator {
@@ -61,142 +54,7 @@ public abstract class AbstractPathStepScalarEvaluator extends AbstractTaggedValu
     }
 
     protected void setNodeTest(SequenceType sType) {
-        final NodeType nodeType = (NodeType) sType.getItemType();
-        switch (nodeType.getNodeKind()) {
-            case ANY:
-                filter = new INodeFilter() {
-                    @Override
-                    public boolean accept(NodeTreePointable ntp, TaggedValuePointable tvp) {
-                        return true;
-                    }
-                };
-                break;
-
-            case ATTRIBUTE: {
-                AttributeType aType = (AttributeType) nodeType;
-                NameTest nameTest = aType.getNameTest();
-                byte[] uri = nameTest.getUri();
-                byte[] localName = nameTest.getLocalName();
-                final UTF8StringPointable urip = (UTF8StringPointable) (uri == null ? null
-                        : UTF8StringPointable.FACTORY.createPointable());
-                final UTF8StringPointable localp = (UTF8StringPointable) (localName == null ? null
-                        : UTF8StringPointable.FACTORY.createPointable());
-                if (uri != null) {
-                    urip.set(uri, 0, uri.length);
-                }
-                if (localName != null) {
-                    localp.set(localName, 0, localName.length);
-                }
-                final IPointable temp = VoidPointable.FACTORY.createPointable();
-                final AttributeNodePointable anp = (AttributeNodePointable) AttributeNodePointable.FACTORY
-                        .createPointable();
-                final CodedQNamePointable cqp = (CodedQNamePointable) CodedQNamePointable.FACTORY.createPointable();
-                filter = new INodeFilter() {
-                    @Override
-                    public boolean accept(NodeTreePointable ntp, TaggedValuePointable tvp) {
-                        if (tvp.getTag() != ValueTag.ATTRIBUTE_NODE_TAG) {
-                            return false;
-                        }
-                        tvp.getValue(anp);
-                        anp.getName(cqp);
-                        if (urip != null) {
-                            ntp.getString(cqp.getNamespaceCode(), temp);
-                            if (urip.compareTo(temp) != 0) {
-                                return false;
-                            }
-                        }
-                        if (localp != null) {
-                            ntp.getString(cqp.getLocalCode(), temp);
-                            if (localp.compareTo(temp) != 0) {
-                                return false;
-                            }
-                        }
-                        return true;
-                    }
-                };
-                break;
-            }
-
-            case COMMENT:
-                filter = new INodeFilter() {
-                    @Override
-                    public boolean accept(NodeTreePointable ntp, TaggedValuePointable tvp) {
-                        return tvp.getTag() == ValueTag.COMMENT_NODE_TAG;
-                    }
-                };
-                break;
-
-            case DOCUMENT:
-                filter = new INodeFilter() {
-                    @Override
-                    public boolean accept(NodeTreePointable ntp, TaggedValuePointable tvp) {
-                        return tvp.getTag() == ValueTag.DOCUMENT_NODE_TAG;
-                    }
-                };
-                break;
-
-            case ELEMENT: {
-                ElementType eType = (ElementType) nodeType;
-                NameTest nameTest = eType.getNameTest();
-                byte[] uri = nameTest.getUri();
-                byte[] localName = nameTest.getLocalName();
-                final UTF8StringPointable urip = (UTF8StringPointable) (uri == null ? null
-                        : UTF8StringPointable.FACTORY.createPointable());
-                final UTF8StringPointable localp = (UTF8StringPointable) (localName == null ? null
-                        : UTF8StringPointable.FACTORY.createPointable());
-                if (uri != null) {
-                    urip.set(uri, 0, uri.length);
-                }
-                if (localName != null) {
-                    localp.set(localName, 0, localName.length);
-                }
-                final IPointable temp = VoidPointable.FACTORY.createPointable();
-                final ElementNodePointable enp = (ElementNodePointable) ElementNodePointable.FACTORY.createPointable();
-                final CodedQNamePointable cqp = (CodedQNamePointable) CodedQNamePointable.FACTORY.createPointable();
-                filter = new INodeFilter() {
-                    @Override
-                    public boolean accept(NodeTreePointable ntp, TaggedValuePointable tvp) {
-                        if (tvp.getTag() != ValueTag.ELEMENT_NODE_TAG) {
-                            return false;
-                        }
-                        tvp.getValue(enp);
-                        enp.getName(cqp);
-                        if (urip != null) {
-                            ntp.getString(cqp.getNamespaceCode(), temp);
-                            if (urip.compareTo(temp) != 0) {
-                                return false;
-                            }
-                        }
-                        if (localp != null) {
-                            ntp.getString(cqp.getLocalCode(), temp);
-                            if (localp.compareTo(temp) != 0) {
-                                return false;
-                            }
-                        }
-                        return true;
-                    }
-                };
-                break;
-            }
-
-            case PI:
-                filter = new INodeFilter() {
-                    @Override
-                    public boolean accept(NodeTreePointable ntp, TaggedValuePointable tvp) {
-                        return tvp.getTag() == ValueTag.PI_NODE_TAG;
-                    }
-                };
-                break;
-
-            case TEXT:
-                filter = new INodeFilter() {
-                    @Override
-                    public boolean accept(NodeTreePointable ntp, TaggedValuePointable tvp) {
-                        return tvp.getTag() == ValueTag.TEXT_NODE_TAG;
-                    }
-                };
-                break;
-        }
+        filter = NodeTestFilter.getNodeTestFilter(sType);
     }
 
     @Override
@@ -206,39 +64,18 @@ public abstract class AbstractPathStepScalarEvaluator extends AbstractTaggedValu
         return filter.accept(ntp, itemTvp);
     }
 
-    protected void appendNodeToResult() throws IOException {
+    protected void setNodeToResult(IPointable result) throws IOException {
         nodeAbvs.reset();
-        DataOutput mainOut = nodeAbvs.getDataOutput();
-        mainOut.write(ValueTag.NODE_TREE_TAG);
-        boolean hasDictionary = ntp.dictionaryExists() && hasDictionary(itemTvp.getTag());
-        byte header = (byte) (hasDictionary ? NodeTreePointable.HEADER_DICTIONARY_EXISTS_MASK : 0);
-		// TODO add all header flags to this setting.
-        boolean hasNodeIds = ntp.nodeIdExists();
-        if (hasNodeIds) {
-            header |= NodeTreePointable.HEADER_NODEID_EXISTS_MASK;
-        }
-        mainOut.write(header);
-        if (hasNodeIds) {
-            mainOut.writeInt(ntp.getRootNodeId());
-        }
-        if (hasDictionary) {
-            mainOut.write(ntp.getByteArray(), ntp.getDictionaryOffset(), ntp.getDictionarySize());
-        }
-        mainOut.write(itemTvp.getByteArray(), itemTvp.getStartOffset(), itemTvp.getLength());
-        seqb.addItem(nodeAbvs);
+        NodeSubTreeBuilder nstb = new NodeSubTreeBuilder();
+        nstb.reset(nodeAbvs);
+        nstb.setChildNode(ntp, itemTvp);
+        nstb.finish();
+        result.set(nodeAbvs.getByteArray(), nodeAbvs.getStartOffset(), nodeAbvs.getLength());
     }
 
-    private boolean hasDictionary(byte tag) {
-        switch (tag) {
-            case ValueTag.ATTRIBUTE_NODE_TAG:
-            case ValueTag.DOCUMENT_NODE_TAG:
-            case ValueTag.ELEMENT_NODE_TAG:
-                return true;
-        }
-        return false;
-    }
-
-    private interface INodeFilter {
-        public boolean accept(NodeTreePointable ntp, TaggedValuePointable tvp);
+    protected void appendNodeToResult() throws IOException {
+        TaggedValuePointable node = (TaggedValuePointable) TaggedValuePointable.FACTORY.createPointable();
+        setNodeToResult(node);
+        seqb.addItem(node);
     }
 }
--- trunk/vxquery/vxquery-core/src/main/java/org/apache/vxquery/runtime/functions/step/ChildPathStepUnnestingEvaluatorFactory.java
+++ trunk/vxquery/vxquery-core/src/main/java/org/apache/vxquery/runtime/functions/step/ChildPathStepUnnestingEvaluatorFactory.java
@@ -16,26 +16,21 @@
  */
 package org.apache.vxquery.runtime.functions.step;
 
-import java.io.DataOutput;
 import java.io.IOException;
 
 import org.apache.vxquery.datamodel.accessors.SequencePointable;
 import org.apache.vxquery.datamodel.accessors.TaggedValuePointable;
-import org.apache.vxquery.datamodel.accessors.atomic.CodedQNamePointable;
-import org.apache.vxquery.datamodel.accessors.nodes.AttributeNodePointable;
 import org.apache.vxquery.datamodel.accessors.nodes.DocumentNodePointable;
 import org.apache.vxquery.datamodel.accessors.nodes.ElementNodePointable;
 import org.apache.vxquery.datamodel.accessors.nodes.NodeTreePointable;
+import org.apache.vxquery.datamodel.builders.nodes.NodeSubTreeBuilder;
 import org.apache.vxquery.datamodel.values.ValueTag;
 import org.apache.vxquery.datamodel.values.XDMConstants;
 import org.apache.vxquery.exceptions.ErrorCode;
 import org.apache.vxquery.exceptions.SystemException;
 import org.apache.vxquery.runtime.functions.base.AbstractTaggedValueArgumentUnnestingEvaluator;
 import org.apache.vxquery.runtime.functions.base.AbstractTaggedValueArgumentUnnestingEvaluatorFactory;
-import org.apache.vxquery.types.AttributeType;
-import org.apache.vxquery.types.ElementType;
-import org.apache.vxquery.types.NameTest;
-import org.apache.vxquery.types.NodeType;
+import org.apache.vxquery.runtime.functions.step.NodeTestFilter.INodeFilter;
 import org.apache.vxquery.types.SequenceType;
 
 import edu.uci.ics.hyracks.algebricks.common.exceptions.AlgebricksException;
@@ -44,8 +39,6 @@ import edu.uci.ics.hyracks.algebricks.runtime.base.IScalarEvaluatorFactory;
 import edu.uci.ics.hyracks.algebricks.runtime.base.IUnnestingEvaluator;
 import edu.uci.ics.hyracks.data.std.api.IPointable;
 import edu.uci.ics.hyracks.data.std.primitive.IntegerPointable;
-import edu.uci.ics.hyracks.data.std.primitive.UTF8StringPointable;
-import edu.uci.ics.hyracks.data.std.primitive.VoidPointable;
 import edu.uci.ics.hyracks.data.std.util.ArrayBackedValueStorage;
 
 public class ChildPathStepUnnestingEvaluatorFactory extends AbstractTaggedValueArgumentUnnestingEvaluatorFactory {
@@ -108,7 +101,7 @@ public class ChildPathStepUnnestingEvaluatorFactory extends AbstractTaggedValueA
                     args[1].getValue(ip);
                     int typeCode = ip.getInteger();
                     SequenceType sType = dCtx.getStaticContext().lookupSequenceType(typeCode);
-                    setNodeTest(sType);
+                    filter = NodeTestFilter.getNodeTestFilter(sType);
                     first = false;
                 }
                 if (args[0].getTag() != ValueTag.NODE_TREE_TAG) {
@@ -119,182 +112,17 @@ public class ChildPathStepUnnestingEvaluatorFactory extends AbstractTaggedValueA
                 seqLength = seqp.getEntryCount();
             }
 
-            protected void setNodeTest(SequenceType sType) {
-                final NodeType nodeType = (NodeType) sType.getItemType();
-                switch (nodeType.getNodeKind()) {
-                    case ANY:
-                        filter = new INodeFilter() {
-                            @Override
-                            public boolean accept(NodeTreePointable ntp, TaggedValuePointable tvp) {
-                                return true;
-                            }
-                        };
-                        break;
-
-                    case ATTRIBUTE: {
-                        AttributeType aType = (AttributeType) nodeType;
-                        NameTest nameTest = aType.getNameTest();
-                        byte[] uri = nameTest.getUri();
-                        byte[] localName = nameTest.getLocalName();
-                        final UTF8StringPointable urip = (UTF8StringPointable) (uri == null ? null
-                                : UTF8StringPointable.FACTORY.createPointable());
-                        final UTF8StringPointable localp = (UTF8StringPointable) (localName == null ? null
-                                : UTF8StringPointable.FACTORY.createPointable());
-                        if (uri != null) {
-                            urip.set(uri, 0, uri.length);
-                        }
-                        if (localName != null) {
-                            localp.set(localName, 0, localName.length);
-                        }
-                        final IPointable temp = VoidPointable.FACTORY.createPointable();
-                        final AttributeNodePointable anp = (AttributeNodePointable) AttributeNodePointable.FACTORY
-                                .createPointable();
-                        final CodedQNamePointable cqp = (CodedQNamePointable) CodedQNamePointable.FACTORY
-                                .createPointable();
-                        filter = new INodeFilter() {
-                            @Override
-                            public boolean accept(NodeTreePointable ntp, TaggedValuePointable tvp) {
-                                if (tvp.getTag() != ValueTag.ATTRIBUTE_NODE_TAG) {
-                                    return false;
-                                }
-                                tvp.getValue(anp);
-                                anp.getName(cqp);
-                                if (urip != null) {
-                                    ntp.getString(cqp.getNamespaceCode(), temp);
-                                    if (urip.compareTo(temp) != 0) {
-                                        return false;
-                                    }
-                                }
-                                if (localp != null) {
-                                    ntp.getString(cqp.getLocalCode(), temp);
-                                    if (localp.compareTo(temp) != 0) {
-                                        return false;
-                                    }
-                                }
-                                return true;
-                            }
-                        };
-                        break;
-                    }
-
-                    case COMMENT:
-                        filter = new INodeFilter() {
-                            @Override
-                            public boolean accept(NodeTreePointable ntp, TaggedValuePointable tvp) {
-                                return tvp.getTag() == ValueTag.COMMENT_NODE_TAG;
-                            }
-                        };
-                        break;
-
-                    case DOCUMENT:
-                        filter = new INodeFilter() {
-                            @Override
-                            public boolean accept(NodeTreePointable ntp, TaggedValuePointable tvp) {
-                                return tvp.getTag() == ValueTag.DOCUMENT_NODE_TAG;
-                            }
-                        };
-                        break;
-
-                    case ELEMENT: {
-                        ElementType eType = (ElementType) nodeType;
-                        NameTest nameTest = eType.getNameTest();
-                        byte[] uri = nameTest.getUri();
-                        byte[] localName = nameTest.getLocalName();
-                        final UTF8StringPointable urip = (UTF8StringPointable) (uri == null ? null
-                                : UTF8StringPointable.FACTORY.createPointable());
-                        final UTF8StringPointable localp = (UTF8StringPointable) (localName == null ? null
-                                : UTF8StringPointable.FACTORY.createPointable());
-                        if (uri != null) {
-                            urip.set(uri, 0, uri.length);
-                        }
-                        if (localName != null) {
-                            localp.set(localName, 0, localName.length);
-                        }
-                        final IPointable temp = VoidPointable.FACTORY.createPointable();
-                        final ElementNodePointable enp = (ElementNodePointable) ElementNodePointable.FACTORY
-                                .createPointable();
-                        final CodedQNamePointable cqp = (CodedQNamePointable) CodedQNamePointable.FACTORY
-                                .createPointable();
-                        filter = new INodeFilter() {
-                            @Override
-                            public boolean accept(NodeTreePointable ntp, TaggedValuePointable tvp) {
-                                if (tvp.getTag() != ValueTag.ELEMENT_NODE_TAG) {
-                                    return false;
-                                }
-                                tvp.getValue(enp);
-                                enp.getName(cqp);
-                                if (urip != null) {
-                                    ntp.getString(cqp.getNamespaceCode(), temp);
-                                    if (urip.compareTo(temp) != 0) {
-                                        return false;
-                                    }
-                                }
-                                if (localp != null) {
-                                    ntp.getString(cqp.getLocalCode(), temp);
-                                    if (localp.compareTo(temp) != 0) {
-                                        return false;
-                                    }
-                                }
-                                return true;
-                            }
-                        };
-                        break;
-                    }
-
-                    case PI:
-                        filter = new INodeFilter() {
-                            @Override
-                            public boolean accept(NodeTreePointable ntp, TaggedValuePointable tvp) {
-                                return tvp.getTag() == ValueTag.PI_NODE_TAG;
-                            }
-                        };
-                        break;
-
-                    case TEXT:
-                        filter = new INodeFilter() {
-                            @Override
-                            public boolean accept(NodeTreePointable ntp, TaggedValuePointable tvp) {
-                                return tvp.getTag() == ValueTag.TEXT_NODE_TAG;
-                            }
-                        };
-                        break;
-                }
-            }
-
             protected boolean matches() {
                 return filter.accept(ntp, itemTvp);
             }
 
             protected void setNodeToResult(IPointable result) throws IOException {
                 nodeAbvs.reset();
-                DataOutput mainOut = nodeAbvs.getDataOutput();
-                mainOut.write(ValueTag.NODE_TREE_TAG);
-                boolean hasDictionary = ntp.dictionaryExists() && hasDictionary(itemTvp.getTag());
-                byte header = (byte) (hasDictionary ? NodeTreePointable.HEADER_DICTIONARY_EXISTS_MASK : 0);
-                // TODO add all header flags to this setting.
-                boolean hasNodeIds = ntp.nodeIdExists();
-                if (hasNodeIds) {
-                    header |= NodeTreePointable.HEADER_NODEID_EXISTS_MASK;
-                }
-                mainOut.write(header);
-                if (hasNodeIds) {
-                    mainOut.writeInt(ntp.getRootNodeId());
-                }
-                if (hasDictionary) {
-                    mainOut.write(ntp.getByteArray(), ntp.getDictionaryOffset(), ntp.getDictionarySize());
-                }
-                mainOut.write(itemTvp.getByteArray(), itemTvp.getStartOffset(), itemTvp.getLength());
+                NodeSubTreeBuilder nstb = new NodeSubTreeBuilder();
+                nstb.reset(nodeAbvs);
+                nstb.setChildNode(ntp, itemTvp);
+                nstb.finish();
                 result.set(nodeAbvs.getByteArray(), nodeAbvs.getStartOffset(), nodeAbvs.getLength());
-            }
-
-            private boolean hasDictionary(byte tag) {
-                switch (tag) {
-                    case ValueTag.ATTRIBUTE_NODE_TAG:
-                    case ValueTag.DOCUMENT_NODE_TAG:
-                    case ValueTag.ELEMENT_NODE_TAG:
-                        return true;
-                }
-                return false;
             }
 
             protected void getSequence(NodeTreePointable ntp, SequencePointable seqp) throws SystemException {
@@ -317,9 +145,4 @@ public class ChildPathStepUnnestingEvaluatorFactory extends AbstractTaggedValueA
 
         };
     }
-
-    private interface INodeFilter {
-        public boolean accept(NodeTreePointable ntp, TaggedValuePointable tvp);
-    }
-
 }
