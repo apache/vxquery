78667b5e49d0e55917d17a5a691d71a808dbd42c
prestonc <prestonc@unknown>
2013-10-16 21:49:20 +0000

    Updated the comment section.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/vxquery/trunk/vxquery@1532921 13f79535-47bb-0310-9956-ffa450edef68

--- vxquery-core/src/main/java/org/apache/vxquery/compiler/rewriter/rules/IntroduceCollectionRule.java
+++ vxquery-core/src/main/java/org/apache/vxquery/compiler/rewriter/rules/IntroduceCollectionRule.java
@@ -47,6 +47,10 @@ import edu.uci.ics.hyracks.algebricks.core.algebra.operators.logical.UnnestOpera
  *    or
  *   ASSIGN( $source : promote( data( $v0 ) ) )
  *   ASSIGN( $v0 : constant )
+ *    or
+ *   ASSIGN( $source : promote( data( $v00 ) ) )
+ *   ASSIGN( $v00 : treat( $v0 ) ) )
+ *   ASSIGN( $v0 : constant )
  *   
  * After 
  * 
