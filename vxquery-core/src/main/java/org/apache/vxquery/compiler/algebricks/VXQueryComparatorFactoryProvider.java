d61f5bd4680fb04b9f6925723f18842f5a45d75d
Preston Carman <prestonc@apache.org>
2014-03-12 13:19:40 -0700

    Removed change to comparator factory provider.
    The change removed all the result for the join operation.

--- vxquery-core/src/main/java/org/apache/vxquery/compiler/algebricks/VXQueryComparatorFactoryProvider.java
+++ vxquery-core/src/main/java/org/apache/vxquery/compiler/algebricks/VXQueryComparatorFactoryProvider.java
@@ -16,9 +16,7 @@
  */
 package org.apache.vxquery.compiler.algebricks;
 
-import org.apache.vxquery.datamodel.accessors.SequencePointable;
 import org.apache.vxquery.datamodel.accessors.TaggedValuePointable;
-import org.apache.vxquery.datamodel.values.ValueTag;
 
 import edu.uci.ics.hyracks.algebricks.common.exceptions.AlgebricksException;
 import edu.uci.ics.hyracks.algebricks.data.IBinaryComparatorFactoryProvider;
@@ -45,44 +43,12 @@ public class VXQueryComparatorFactoryProvider implements IBinaryComparatorFactor
         public IBinaryComparator createBinaryComparator() {
             final TaggedValuePointable tvp1 = (TaggedValuePointable) TaggedValuePointable.FACTORY.createPointable();
             final TaggedValuePointable tvp2 = (TaggedValuePointable) TaggedValuePointable.FACTORY.createPointable();
-            final SequencePointable sp1 = (SequencePointable) SequencePointable.FACTORY.createPointable();
-            final SequencePointable sp2 = (SequencePointable) SequencePointable.FACTORY.createPointable();
             return new IBinaryComparator() {
                 @Override
                 public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
                     tvp1.set(b1, s1, l1);
                     tvp2.set(b2, s2, l2);
-                    if (tvp1.getTag() != tvp2.getTag()) {
-                        return tvp1.getTag() - tvp2.getTag();
-                    }
-                    // Empty sequences do not match. Supports hash based join.
-                    switch (tvp1.getTag()) {
-                        case ValueTag.SEQUENCE_TAG:
-                            tvp1.getValue(sp1);
-                            if (sp1.getEntryCount() == 0) {
-                                return -1;
-                            }
-
-                            switch (tvp2.getTag()) {
-                                case ValueTag.SEQUENCE_TAG:
-                                    tvp2.getValue(sp2);
-                                    if (sp2.getEntryCount() == 0) {
-                                        return 1;
-                                    }
-                                    break;
-                                default:
-                            }
-
-                            break;
-                        default:
-                    }
-                    // Do a binary compare between byte arrays.
-                    for (int i = 0; i < b1.length && i < b2.length; i++) {
-                        if (b1[i] != b2[i]) {
-                            return b1[i] - b2[i];
-                        }
-                    }
-                    return b1.length - b2.length;
+                    return 0;
                 }
             };
         }
