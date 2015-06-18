fa890b27597af1b6137fd284694e73cbb8ecab6a
Shivani Mall <small002@ucr.edu>
2015-06-17 17:46:51 -0700

    Apache License header added to new files

--- vxquery-core/src/main/java/org/apache/vxquery/runtime/functions/node/AbstractNodePositionalCheckEvaluator.java
+++ vxquery-core/src/main/java/org/apache/vxquery/runtime/functions/node/AbstractNodePositionalCheckEvaluator.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.vxquery.runtime.functions.node;
 
 import org.apache.vxquery.datamodel.accessors.TaggedValuePointable;
@@ -36,7 +53,6 @@ public abstract class AbstractNodePositionalCheckEvaluator extends AbstractTagge
         }
     }
 
-    //node passed in from argument
     protected void parameterTypeCheck(TaggedValuePointable node, NodeTreePointable ntp) throws SystemException {
         if (node.getTag() == ValueTag.NODE_TREE_TAG) {
             node.getValue(ntp);
