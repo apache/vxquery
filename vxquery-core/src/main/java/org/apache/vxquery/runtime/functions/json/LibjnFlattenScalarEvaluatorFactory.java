f5fb647671a63506d67f4c83376eb206eb86013a
Christina Pavlopoulou <cpavl001@ucr.edu>
2016-07-20 10:37:07 -0700

    Working version of libjn:flatten

--- vxquery-core/src/main/java/org/apache/vxquery/runtime/functions/json/LibjnFlattenScalarEvaluatorFactory.java
+++ vxquery-core/src/main/java/org/apache/vxquery/runtime/functions/json/LibjnFlattenScalarEvaluatorFactory.java
@@ -23,12 +23,10 @@ import org.apache.hyracks.algebricks.runtime.base.IScalarEvaluator;
 import org.apache.hyracks.algebricks.runtime.base.IScalarEvaluatorFactory;
 import org.apache.hyracks.api.context.IHyracksTaskContext;
 import org.apache.hyracks.data.std.api.IPointable;
-import org.apache.hyracks.data.std.primitive.UTF8StringPointable;
 import org.apache.hyracks.data.std.util.ArrayBackedValueStorage;
 import org.apache.vxquery.datamodel.accessors.SequencePointable;
 import org.apache.vxquery.datamodel.accessors.TaggedValuePointable;
 import org.apache.vxquery.datamodel.accessors.jsonitem.ArrayPointable;
-import org.apache.vxquery.datamodel.accessors.jsonitem.ObjectPointable;
 import org.apache.vxquery.datamodel.builders.sequence.SequenceBuilder;
 import org.apache.vxquery.datamodel.values.ValueTag;
 import org.apache.vxquery.exceptions.ErrorCode;
@@ -50,8 +48,6 @@ public class LibjnFlattenScalarEvaluatorFactory extends AbstractTaggedValueArgum
         final SequencePointable sp = (SequencePointable) SequencePointable.FACTORY.createPointable();
         final ArrayPointable ap = (ArrayPointable) ArrayPointable.FACTORY.createPointable();
         final ArrayPointable ap1 = (ArrayPointable) ArrayPointable.FACTORY.createPointable();
-        final ObjectPointable op = (ObjectPointable) ObjectPointable.FACTORY.createPointable();
-        final UTF8StringPointable stringp = (UTF8StringPointable) UTF8StringPointable.FACTORY.createPointable();
         final SequenceBuilder sb = new SequenceBuilder();
         final ArrayBackedValueStorage abvs = new ArrayBackedValueStorage();
 
@@ -68,7 +64,7 @@ public class LibjnFlattenScalarEvaluatorFactory extends AbstractTaggedValueArgum
                 for (int i = 0; i < size; i++) {
                     sp.getEntry(i, tempTvp);
                     if (tempTvp.getTag() == ValueTag.ARRAY_TAG) {
-                        flatten(tempTvp);
+                        flatten(tempTvp, ap);
                     } else {
                         try {
                             sb.addItem(tempTvp);
@@ -85,45 +81,32 @@ public class LibjnFlattenScalarEvaluatorFactory extends AbstractTaggedValueArgum
                 }
             }
 
-            public TaggedValuePointable flatten(TaggedValuePointable tvp) throws SystemException {
+            public void flatten(TaggedValuePointable tvp, ArrayPointable ap) throws SystemException {
                 TaggedValuePointable tvp1 = ppool.takeOne(TaggedValuePointable.class);
-                boolean inArray = false;
                 tvp.getValue(ap);
-                ap.getEntry(0, tvp1);
-                if (tvp1.getTag() == ValueTag.ARRAY_TAG) {
-                    inArray = true;
-                }
-                if(!inArray){
-                    appendSequence(tvp, ap);
-                }
                 int size = ap.getEntryCount();
-                if (size > 1) {
-                    for (int i = 1; i < size; i++) {
-                        if (inArray) {
-                            appendSequence(tvp1, ap1);
-                            ap.getEntry(i, tvp1);
-                            if (tvp1.getTag() == ValueTag.ARRAY_TAG) {
-                                inArray = true;
-                            }
+                for (int i = 0; i < size; i++) {
+                    ap.getEntry(i, tvp1);
+                    if (tvp1.getTag() == ValueTag.ARRAY_TAG) {
+                        flatten(tvp1, ap1);
+                    } else {
+                        try {
+                            sb.addItem(tvp1);
+                        } catch (IOException e) {
+                            throw new SystemException(ErrorCode.SYSE0001, e);
                         }
                     }
                 }
-                ppool.giveBack(tvp1);
-                if (inArray) {
-                    return flatten(tvp1);
-                } else {
-                    return null;
-                }
             }
 
-            public void appendSequence(TaggedValuePointable tvp, ArrayPointable ap) throws SystemException {
-                tvp.getValue(ap);
-                try {
-                    ap.appendItems(sb);
-                } catch (IOException e1) {
-                    throw new SystemException(ErrorCode.SYSE0001, e1);
-                }
-            }
+            //            public void appendSequence(TaggedValuePointable tvp, ArrayPointable ap) throws SystemException {
+            //                tvp.getValue(ap);
+            //                try {
+            //                    ap.appendItems(sb);
+            //                } catch (IOException e1) {
+            //                    throw new SystemException(ErrorCode.SYSE0001, e1);
+            //                }
+            //            }
 
         };
     }
